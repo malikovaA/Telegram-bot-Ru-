ada7295d875026231e61a907653a16526a42b525
Alexandra_Malikova <malikova.aleksandra@yandex.ru>
2022-05-21 15:08:29 +0500

    inlime keyboard

--- Russian_bot.py
+++ Russian_bot.py
@@ -9,6 +9,10 @@ bot = telebot.TeleBot(token)
 # for message1 in util.smart_split(text, 1000):   # метод util.smart_split позволяет присылать большие тексты
 #     bot.send_message(message.chat.id, message1)
 
+# @bot.message_handler(commands=['start'])
+# def start(message):
+#     bot.send_poll(message.chat.id, question='Как дела?', options=['Хорошо', 'Нормально', 'Плохо'],
+#                   allows_multiple_answers=False, is_anonymous=True)
 
 # @bot.message_handler()
 # def name(message):
@@ -44,8 +48,6 @@ def subject(callback):
         kb.add(lexis, word, orthography, parts_of_speech)
         bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id, text='Выберите тему.',
                               reply_markup=kb)
-
-def parts_of_speech(callback):
     if callback.data == 'parts_of_speech':
         prt = types.InlineKeyboardMarkup(row_width=1)
         theory = types.InlineKeyboardButton(text='ТЕОРИЯ', callback_data='theory')
@@ -54,6 +56,7 @@ def parts_of_speech(callback):
         prt.add(theory, tests, results)
         bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id, text='Выберите раздел.',
                                   reply_markup=prt)
+
 # @bot.callback_query_handler(func=lambda callback: callback.data)
 # def parts_of_speech(callback):
 #     if callback.data == 'parts_of_speech':
