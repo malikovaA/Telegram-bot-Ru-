0978614531771e8dd42c7aa1ae209ccfb7a9474d
Da_Maltsev <danil.zlatoust9999@gmail.com>
2022-05-22 15:26:41 +0500

    teacher role

--- Russian_bot.py
+++ Russian_bot.py
@@ -27,23 +27,91 @@ def start(message):
     bot.send_message(message.chat.id, 'Добро пожаловать в чат-бот по русскому языку для 6-9 классов!\n'
                                       'Данный бот поможет вам освежить свои знания в рамках школьной программы и проверить их с помощью тестов.\n'
                                       'Для продолжения работы <b> введите пароль </b>. \n'
-                                      'Узнать его можно у своего преподавателя по русскому языку.', parse_mode='HTML')
-    @bot.message_handler(func=lambda m: m.text == '01035')
+                                      'Узнать его можно у своего преподавателя по русскому языку.\n'
+                                      'Для ученика пароль 1111, для учителя 0000.', parse_mode='HTML')
+    @bot.message_handler(func=lambda m: m.text == '1111')
     def start(message):
         startKBoard = types.InlineKeyboardMarkup(row_width=1)
         startKBoard.add(*[types.InlineKeyboardButton(text=name, callback_data='back') for name in db_worker.grades])
         bot.send_message(message.chat.id, 'Выберите свой класс.', reply_markup=startKBoard)
 
+    @bot.message_handler(func=lambda m: m.text == '0000')
+    def start(message):
+        startKBoard = types.InlineKeyboardMarkup(row_width=1)
+        startKBoard.add(*[types.InlineKeyboardButton(text=name, callback_data='teacher') for name in db_worker.grades])
+        bot.send_message(message.chat.id, 'Здравствуйте, учитель, выберите класс.', reply_markup=startKBoard)
+
 @bot.callback_query_handler(func=lambda callback: callback.data)
 def subject(callback):
     count = 0
-    if callback.data in ['sig', 'seg', 'eg', 'ng', 'back']:
+    if callback.data in ['teacher', 'back_t']:
         kb = types.InlineKeyboardMarkup(row_width=1)
-        kb.add(*[types.InlineKeyboardButton(text=name, callback_data='lexis') for name in db_worker.themes])
+        stat = types.InlineKeyboardButton(text='Статистика', callback_data='stat_t')
+        theme = types.InlineKeyboardButton(text='Выберите тему', callback_data='theme_choose_t')
+        add_theme = types.InlineKeyboardButton(text='Добавить тему', callback_data='add_theme')
+        kb.add(stat, theme, add_theme)
+        bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id, text='Выберите опцию.',
+                              reply_markup=kb)
+    if callback.data in ['stat_t']:
+        text = f'Тестовый текст с результатами класса.\nУ вас всё в шоколаде {10}/{10}'
+        kb = types.InlineKeyboardMarkup(row_width=1)
+        back_t = types.InlineKeyboardButton(text='Назад', callback_data='back_t')
+        kb.add(back_t)
+        bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id, text=text,
+                              reply_markup=kb)
+    if callback.data in ['add_theme']:
+        kb = types.InlineKeyboardMarkup(row_width=1)
+        back_t = types.InlineKeyboardButton(text='Назад', callback_data='back_t')
+        kb.add(back_t)
+        bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id, text='Введите название темы',
+                              reply_markup=kb)
+    if callback.data in ['theme_choose_t']:
+        kb = types.InlineKeyboardMarkup(row_width=1)
+        back_t = types.InlineKeyboardButton(text='Назад', callback_data='back_t')
+        kb.add(*[types.InlineKeyboardButton(text=name, callback_data='theme_t') for name in db_worker.themes])
+        kb.add(back_t)
+        bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id, text='Выберите тему.',
+                              reply_markup=kb)
+    if callback.data in ['theme_t']:
+        kb = types.InlineKeyboardMarkup(row_width=1)
+        delete = types.InlineKeyboardButton(text='Удалить тему', callback_data='delete_t')
+        import_t = types.InlineKeyboardButton(text='Добавить тест', callback_data='import_t')
+        stat = types.InlineKeyboardButton(text='Статистика по теме', callback_data='stat_t_theme')
+        test_t = types.InlineKeyboardButton(text='Выбор теста', callback_data='test_t')
+        back_t = types.InlineKeyboardButton(text='Назад', callback_data='back_t')
+        kb.add(import_t, stat, delete, test_t,back_t)
+        bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id,
+                              text='Выберите, что нужно сделать для этой темы. ',
+                              reply_markup=kb)
+    if callback.data in ['import_t']:
+        kb = types.InlineKeyboardMarkup(row_width=1)
+        back_t = types.InlineKeyboardButton(text='Назад', callback_data='back_t')
+        kb.add(back_t)
+        bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id, text='Добавьте файл установленного формата с тестом',
+                              reply_markup=kb)
+    if callback.data in ['stat_t_theme']:
+        text = f'Тестовый текст с результатами по теме.\nУ вас всё в шоколаде {10}/{10}'
+        kb = types.InlineKeyboardMarkup(row_width=1)
+        back_t = types.InlineKeyboardButton(text='Назад', callback_data='back_t')
+        kb.add(back_t)
+        bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id, text=text,
+                              reply_markup=kb)
+    if callback.data in ['test_t']:
+        kb = types.InlineKeyboardMarkup(row_width=1)
+        text = f'Тестовый текст с потенциальным названием теста'
+        delete = types.InlineKeyboardButton(text='Удалить тест', callback_data='delete_t')
+        back_t = types.InlineKeyboardButton(text='Назад', callback_data='back_t')
+        kb.add(delete, back_t)
+        bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id,
+                              text=text,
+                              reply_markup=kb)
+    if callback.data in ['back']:
+        kb = types.InlineKeyboardMarkup(row_width=1)
+        kb.add(*[types.InlineKeyboardButton(text=name, callback_data='theme_s') for name in db_worker.themes])
         bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id, text='Выберите тему.',
                               reply_markup=kb)
         count = 0
-    if callback.data in ['parts_of_speech', 'lexis', 'word', 'orthography']: # ВОТ ТУТ ЕЩЕ ДО УМА ДБ ДОВЕСТИ НАДО
+    if callback.data in ['theme_s']: # ВОТ ТУТ ЕЩЕ ДО УМА ДБ ДОВЕСТИ НАДО
         prt = types.InlineKeyboardMarkup(row_width=1)
         theory = types.InlineKeyboardButton(text='ТЕОРИЯ', callback_data='theory')
         tests = types.InlineKeyboardButton(text='ТЕСТЫ', callback_data='tests')
