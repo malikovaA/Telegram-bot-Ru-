1cbf11f009354ea20036b9585c30d4a666109063
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2022-05-23 10:11:48 +0500

    comments and db upgrade

--- Russian_bot.py
+++ Russian_bot.py
@@ -44,6 +44,8 @@ def start(message):
 @bot.callback_query_handler(func=lambda callback: callback.data)
 def subject(callback):
     count = 0
+
+    """ Главное меню для учителя """
     if callback.data in ['teacher', 'back_t']:
         kb = types.InlineKeyboardMarkup(row_width=1)
         stat = types.InlineKeyboardButton(text='Статистика', callback_data='stat_t')
@@ -52,6 +54,7 @@ def subject(callback):
         kb.add(stat, theme, add_theme)
         bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id, text='Выберите опцию.',
                               reply_markup=kb)
+        """ Статистика по классу """
     if callback.data in ['stat_t']:
         text = f'Тестовый текст с результатами класса.\nУ вас всё в шоколаде {10}/{10}'
         kb = types.InlineKeyboardMarkup(row_width=1)
@@ -59,12 +62,16 @@ def subject(callback):
         kb.add(back_t)
         bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id, text=text,
                               reply_markup=kb)
+
+        """ Добавление темы """
     if callback.data in ['add_theme']:
         kb = types.InlineKeyboardMarkup(row_width=1)
         back_t = types.InlineKeyboardButton(text='Назад', callback_data='back_t')
         kb.add(back_t)
         bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id, text='Введите название темы',
                               reply_markup=kb)
+
+        """ Меню с выбором тем для взаимодействия"""
     if callback.data in ['theme_choose_t']:
         kb = types.InlineKeyboardMarkup(row_width=1)
         back_t = types.InlineKeyboardButton(text='Назад', callback_data='back_t')
@@ -72,6 +79,8 @@ def subject(callback):
         kb.add(back_t)
         bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id, text='Выберите тему.',
                               reply_markup=kb)
+
+        """ Меню с выбором опций по взаимодействию с выбранной темой """
     if callback.data in ['theme_t']:
         kb = types.InlineKeyboardMarkup(row_width=1)
         delete = types.InlineKeyboardButton(text='Удалить тему', callback_data='delete_t')
@@ -83,12 +92,16 @@ def subject(callback):
         bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id,
                               text='Выберите, что нужно сделать для этой темы. ',
                               reply_markup=kb)
+
+        """ Раздел для импорта теста """
     if callback.data in ['import_t']:
         kb = types.InlineKeyboardMarkup(row_width=1)
         back_t = types.InlineKeyboardButton(text='Назад', callback_data='back_t')
         kb.add(back_t)
         bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id, text='Добавьте файл установленного формата с тестом',
                               reply_markup=kb)
+
+        """ Раздел со статистикой по теме """
     if callback.data in ['stat_t_theme']:
         text = f'Тестовый текст с результатами по теме.\nУ вас всё в шоколаде {10}/{10}'
         kb = types.InlineKeyboardMarkup(row_width=1)
@@ -96,6 +109,8 @@ def subject(callback):
         kb.add(back_t)
         bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id, text=text,
                               reply_markup=kb)
+
+        """ Меню для просмотра / удаления теста """
     if callback.data in ['test_t']:
         kb = types.InlineKeyboardMarkup(row_width=1)
         text = f'Тестовый текст с потенциальным названием теста'
@@ -105,13 +120,16 @@ def subject(callback):
         bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id,
                               text=text,
                               reply_markup=kb)
+
+        """ Главное меню для ученика """
     if callback.data in ['back']:
         kb = types.InlineKeyboardMarkup(row_width=1)
         kb.add(*[types.InlineKeyboardButton(text=name, callback_data='theme_s') for name in db_worker.themes])
         bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id, text='Выберите тему.',
                               reply_markup=kb)
         count = 0
-    if callback.data in ['theme_s']: # ВОТ ТУТ ЕЩЕ ДО УМА ДБ ДОВЕСТИ НАДО
+        """ Меню с выбором раздела по теме """
+    if callback.data in ['theme_s']:
         prt = types.InlineKeyboardMarkup(row_width=1)
         theory = types.InlineKeyboardButton(text='ТЕОРИЯ', callback_data='theory')
         tests = types.InlineKeyboardButton(text='ТЕСТЫ', callback_data='tests')
@@ -120,6 +138,8 @@ def subject(callback):
         prt.add(theory, tests, results, back)
         bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id, text='Выберите раздел.',
                                   reply_markup=prt)
+
+        """ Вкладка с теорией """
     if callback.data == 'theory':
         sources = types.InlineKeyboardMarkup(row_width=1)
         back = types.InlineKeyboardButton(text='Назад', callback_data='back')
@@ -127,6 +147,8 @@ def subject(callback):
         sources.add(back)
         bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id, text='Ознакомтесь с источниками.',
                                   reply_markup=sources)
+
+        """ Раздел для просмотра результатов """
     if callback.data == 'results':
         text = f'Результат последнего теста {randint(0,10)} баллов'
         sources = types.InlineKeyboardMarkup(row_width=1)
@@ -136,6 +158,11 @@ def subject(callback):
         sources.add(theory, test_last, back)
         bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id, text='Вы - капитальный красавчик.',
                                   reply_markup=sources)
+
+        """ Вкладка для выбора теста 
+        Но тут еще надо будет внести правки 
+        по принципу работы тестов        
+        """
     if callback.data == 'tests':
         sources = types.InlineKeyboardMarkup(row_width=1)
         test1 = types.InlineKeyboardButton(text='Блок тестов', url='https://yandex.ru/tutor/subject/?subject_id=3')
