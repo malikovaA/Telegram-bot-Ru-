31fbabd162a3bad0b71b3ba8bb29c6dd6346ba04
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2022-05-30 10:10:40 +0500

    import wip

--- db/sql_methods.py
+++ db/sql_methods.py
@@ -1,4 +1,4 @@
-# from sqlalch import Class, Student, Theme, Test_name, Test_answer, Test_question, Result, Theory
+from sqlalch import session, Class, Student, Theme, Test_name, Test_answer, Test_question, Result, Theory
 from openpyxl import load_workbook
 
 # import telebot
@@ -16,20 +16,34 @@ def get_or_create(session, model, **kwargs):
         session.commit()
         return instance
 
-""" 
-    Пока эта функция просто читает каждую строку файла Excel
-    Идея - учитель записывает тест (вопросы и ответы)
-    в правильном порядке в Excel файл и с помощью
-    этой функции мы его парсим, а потом полученные
-    данные записываем в БД в таблицы 
-    Test_names, Test_questions, Test_answers   
-"""
 
+'''В функцию передаётся название файла (получаем из метода telebot по полученному файлу)'''
 def import_test(doc_name):
-    doc_name = 'test.xlsx'
-    wb = load_workbook('files/'+doc_name)
+    wb = load_workbook(filename=doc_name)
     sheet = wb.active
+    new_test = Test_name(title=doc_name) #Создается новый экземпляр теста со значением имени файла (название теста)
+    session.add(new_test)
+    session.commit()
+    '''Чтение каждой строки'''
     for row in sheet:
-        print('row')
-        for cell in row:
-            print(cell.value)
+        '''Внутри каждой строки происходит чтение каждой ячейки'''
+        new_question = Test_question()
+        for count, cell in enumerate(row):
+            '''В первой ячейке хранится вопрос'''
+            if count == 0:
+                new_question = Test_question(content=cell.value, test_id=new_test.id)
+                session.add(new_question)
+                session.commit()
+                '''Следующие 3 ячейки содержат неправильные ответы, значение right по умолчанию false'''
+            elif 0 < count < 4 :
+                new_answer = Test_answer(content=cell.value, test_q_id=new_question.id)
+                session.add(new_answer)
+                session.commit()
+                '''В пятой ячейке всегда должен быть правильный ответ, его записываем с параметром right 1'''
+            elif count == 4:
+                new_answer = Test_answer(content=cell.value, right=1, test_q_id=new_question.id)
+                session.add(new_answer)
+                session.commit()
+            return 'Тест успешно импортирован'
+
+import_test('test.xlsx')
